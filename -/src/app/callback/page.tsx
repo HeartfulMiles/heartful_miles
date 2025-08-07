b9726f628a60ce62562d688c8c8db03b49b54ea2
HeartfulMiles <heartfulmiles@gmail.com>
2025-08-07 16:31:56 +0530

    Add files via upload

---
+++ -/src/app/callback/page.tsx
@@ -0,0 +1,8 @@
+// import { useSearchParams } from "next/navigation";
+
+export default function Callback() {
+    // const searchParams = useSearchParams();
+    // const code = searchParams.get("code");
+    // console.log(code);
+    return <div>Callback</div>;
+}
